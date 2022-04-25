52cc73dabc626bdda3a79e41d95d7a3d515cfac9
Karpukhin-EV <evkarpukhin@mail.ru>
2022-04-25 21:40:01 +0300

    Initial commit

---
+++ src/main/java/ru/skyeng/skyprospringhw2_11/Application.java
@@ -0,0 +1,13 @@
+package ru.skyeng.skyprospringhw2_11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/ru/skyeng/skyprospringhw2_11/controller/MyController.java
@@ -0,0 +1,7 @@
+package ru.skyeng.skyprospringhw2_11.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+}
